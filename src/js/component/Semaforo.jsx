aaf0bdc210cb0e4ea9b4917e5ce6faa0c5b2826c
Tavoide <gustavobenitez116@gmail.com>
2024-12-20 21:31:35 +0000

    semaforo terminado

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,24 +1,57 @@
 import React, {useState} from "react";
-import './Semaforo.css'
 
-function Semaforo() {
-   const [color, setColor]=useState{red};
-    return (
-      <div className="semaforo">
-     <div onClick={()=> setColor(color)}
-     className={color === "red"? "red light" : "light"}
-     ></div>
-      
-     <div onClick={()=> setColor(color)}
-     className={color === "red"? "red light" : "light"}
-     ></div>
 
-     <div onClick={()=> setColor(color)}
-     className={color === "red"? "red light" : "light"}
-     ></div>
-     </div>
-    );
-};
+const Semaforo = () => {
+  const [activarColor, setActiveColor] =useState(null);
+
+  const colorDeLuz = (color) => { setActiveColor (color)};
+
+
+return (
+  <div className="d-flex justify-content-center align-items-center mt-5">
+      <div
+        className="bg-dark rounded d-flex flex-column justify-content-around p-2"
+        style={{
+          width: '100px',
+          height: '300px',
+        }}
+      >
+        <div
+          onClick={() => colorDeLuz('red')}
+          className={`rounded-circle mx-auto ${
+            activarColor === 'red' ? 'bg-danger' : 'bg-secondary'
+          }`}
+          style={{
+            width: '60px',
+            height: '60px',
+          }}
+        ></div>
+
+        <div
+          onClick={() => colorDeLuz('yellow')}
+          className={`rounded-circle mx-auto ${
+            activarColor === 'yellow' ? 'bg-warning' : 'bg-secondary'
+          }`}
+          style={{
+            width: '60px',
+            height: '60px',
+          }}
+        ></div>
+
+        <div
+          onClick={() => colorDeLuz('green')}
+          className={`rounded-circle mx-auto ${
+            activarColor === 'green' ? 'bg-success' : 'bg-secondary'
+          }`}
+          style={{
+            width: '60px',
+            height: '60px',
+          }}
+        ></div>
+      </div>
+    </div>
+);
+}
 
 
 
